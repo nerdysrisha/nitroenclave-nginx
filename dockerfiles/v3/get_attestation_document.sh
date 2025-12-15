0d58e171737a61cf11e61e953884a6684ac264bf
nerdysrisha <nerdysrisha@gmail.com>
2025-12-15 18:36:39 +0000

    Update get_attestation_document.sh

--- dockerfiles/v3/get_attestation_document.sh
+++ dockerfiles/v3/get_attestation_document.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-# Read base64 nonce from stdin and call Rust attestation
+
+# Read base64-encoded nonce from stdin (sent by parent)
 read -r NONCE_B64
-echo "$NONCE_B64" | /opt/attest
+
+# Call the attestation binary with the nonce
+# The binary should generate the attestation document including this nonce
+ATTESTATION_JSON=$(/opt/attest/attest_c "$NONCE_B64")
+
+# Output the attestation document to stdout
+echo "$ATTESTATION_JSON"
