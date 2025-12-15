41222b375e4affd021b444261a4bfe066c07a81d
nerdysrisha <nerdysrisha@gmail.com>
2025-12-15 18:53:42 +0000

    Update get_attestation_document.sh

--- dockerfiles/v3/get_attestation_document.sh
+++ dockerfiles/v3/get_attestation_document.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-# Read base64-encoded nonce from stdin (sent by parent)
 read -r NONCE_B64
-
-# Call the attestation binary with the nonce
-# The binary should generate the attestation document including this nonce
-ATTESTATION_JSON=$(/opt/attest/attest_c "$NONCE_B64")
-
-# Output the attestation document to stdout
-echo "$ATTESTATION_JSON"
+echo "$NONCE_B64" | /opt/attest_c
